43ec8968fa9c22b39549e9cafa4470b5ddf39b97
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2022-08-28 18:41:19 -0400

    commit tree 7669f1ce26b7d019fbe968b94fcc93cab1185d6a

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
-# Your code here!
+
+def greet_programmer()
+    puts "Hello, programmer"
+end
+greet_programmer()
+
+def greet name
+    puts "Hello, #{name}!"
+end
+greet("Thierry")
+
+
+def greet_with_default
+    name="programmer"
+puts "Hello, #{name}"
+end
+greet_with_default()
+
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+def add (num1, num2)
+    return num1 + num2
+end
+add()
